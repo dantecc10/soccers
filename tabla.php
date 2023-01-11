768ae9767004494a7036e180cf3bc59d681646de
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-01-11 12:44:07 -0600

    lógica de bucles y contenedores

--- tabla.php
+++ tabla.php
@@ -7,22 +7,24 @@ $resultado = mysqli_query($conexión, $consulta) or die("Error en la consulta a 
 $contador = 0;
 
 $campos = ['id', 'nombre', 'descripción', 'precio', 'existencia', 'color', 'activo'];
-echo "<table>";
-echo "<tr>";
-
 $tablaSQL = "productos";
+
+echo "<table>"; //Súpercontenedor
+echo "<tr>"; //Contenedor
 
 for ($i = 1; $i < count($campos); $i++) {
     echo "<th>" . $campos[$i] . "</th>";
     $camposSQL[$i] = ($campos[$i] . "_" . substr_replace($tablaSQL, "", -1));
 }
-echo "</tr>";
+
+echo "</tr>"; //Contenedor
+
 while ($columna = mysqli_fetch_array($resultado)) {
-    echo "<tr>";
+    echo "<tr>"; //Contenedor
     for ($i = 1; $i < count($campos); $i++) {
         echo "<td>" . $columna[$camposSQL[$i]] . "</td>";
     }
-    echo "</tr>";
+    echo "</tr>"; //Contenedor
 }
 
 echo "</table>";
