638b7ff6c5f05c0a71dcffa3ae3afd4ba33afb8d
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-01-13 09:23:53 -0600

    borro 1

--- index.php
+++ index.php
@@ -29,14 +29,6 @@
                 <div class="col-xl-9 mx-auto position-relative">
                     <h1 class="mb-5">Compras con envíos a domicilio</h1>
                 </div>
-                <div class="col-md-10 col-lg-8 col-xl-7 mx-auto position-relative">
-
-                    <div class="row">
-                        <div class="col-12 col-md-9 mb-2 mb-md-0"><input class="form-control form-control-lg" id="capturaBúsqueda" name="capturaBúsqueda" type="search" onkeydown="javascript:buscarProductos();" onkeyup="javascript:buscarProductos();" placeholder="Buscar productos"></div>
-                        <div class="col-12 col-md-3"><button class="btn btn-primary btn-lg" type="submit">Buscar</button></div>
-                    </div>
-
-                </div>
             </div>
         </div>
     </header>
