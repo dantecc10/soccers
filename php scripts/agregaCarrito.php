4db6df6b0d4ad98f27d1479aa7a621dfdcdb5d20
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-05-26 23:45:47 -0600

    igual

--- php scripts/agregaCarrito.php
+++ php scripts/agregaCarrito.php
@@ -15,6 +15,6 @@ $_SESSION['carrito'][(sizeof($_SESSION['carrito']['cantidad'])) + 1] = $cantidad
 
 
 echo ("Los artículos del carrito son: ");
-for ($i = 0; $i < sizeof($_SESSION['carrito']['id']); $i++) {
+for ($i = 0; $i = sizeof($_SESSION['carrito']['id']); $i++) {
     echo ("<p>" . $_SESSION['carrito']['id'][$i] . ": " . $_SESSION['carrito']['cantidad'][$i] . "</p>");
 }
