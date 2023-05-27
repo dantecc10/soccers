38bd1264b6df30f221e2067c1cfa2ce18c2aed6d
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-05-27 00:14:28 -0600

    ¿orden de factores?

--- php scripts/agregaCarrito.php
+++ php scripts/agregaCarrito.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
+$_SESSION['carrito'];
+$_SESSION['carrito']['id'] = [];
+$_SESSION['carrito']['cantidad'] = [];
 
 $id = $_GET['id'];
 $cantidad = $_GET['cantidad'];
 
-$_SESSION['carrito'];
-$_SESSION['carrito']['id'] = [];
-$_SESSION['carrito']['cantidad'] = [];
 
 
 $_SESSION['carrito']['id'][(sizeof($_SESSION['carrito']['id']))] = $id;
