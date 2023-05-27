dc04b1c0c9ee58533d22faee9f4348641f68bf7e
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-05-26 23:44:08 -0600

    testing

---
+++ php scripts/agregaCarrito.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+$id = $_GET['id'];
+$cantidad = $_GET['cantidad'];
+
+$_SESSION['carrito'];
+$_SESSION['carrito']['id'] = [""];
+$_SESSION['carrito']['cantidad'] = [""];
+
+
+$_SESSION['carrito'][(sizeof($_SESSION['carrito']['id'])) + 1] = $id;
+$_SESSION['carrito'][(sizeof($_SESSION['carrito']['cantidad'])) + 1] = $cantidad;
+
+
+
+echo ("Los artículos del carrito son: ");
+for ($i = 0; $i < sizeof($_SESSION['carrito']['id']); $i++) {
+    echo ("<p>" . $_SESSION['carrito']['id'][$i] . ": " . $_SESSION['carrito']['cantidad'][$i] . "</p>");
+}
